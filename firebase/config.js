5b917e50a0226b2bc826242a1d88eb0cd0a62cdf
mhanna15 <minaghanna@gmail.com>
2020-05-10 21:37:38 -0700

    set up firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+export default {
+    FirebaseConfig: {
+    apiKey: "AIzaSyDqPIvTOhXmFY0SaefeJzoYINMdSnBjh3w",
+    authDomain: "virustrack-5b1ed.firebaseapp.com",
+    databaseURL: "https://virustrack-5b1ed.firebaseio.com",
+    projectId: "virustrack-5b1ed",
+    storageBucket: "virustrack-5b1ed.appspot.com",
+    messagingSenderId: "1077699388547",
+    appId: "1:1077699388547:web:850d30c26bd98aac8b27b6"
+  }
+}
